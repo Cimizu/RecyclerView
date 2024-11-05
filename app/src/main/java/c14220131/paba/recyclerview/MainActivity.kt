fea49ba8b383efee451ba3270801e7bf5e046ce3
Cimizu <gracienne24@gmail.com>
2024-11-05 20:36:38 +0700

    Initial commit

---
+++ app/src/main/java/c14220131/paba/recyclerview/MainActivity.kt
@@ -0,0 +1,59 @@
+package c14220131.paba.recyclerview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var _nama : Array<String>
+    private lateinit var _karakter : Array<String>
+    private lateinit var _deskripsi : Array<String>
+    private lateinit var _gambar : Array<String>
+    private lateinit var _rvWayang : RecyclerView
+
+    private var arWayag = arrayListOf<wayang>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
+        SiapkanData()
+        TambahData()
+        TampilkanData()
+
+    }
+
+    fun SiapkanData(){
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    fun TambahData(){
+        for (posititon in _nama.indices){
+            val data = wayang(
+                _gambar[posititon],
+                _nama[posititon],
+                _karakter[posititon],
+                _deskripsi[posititon]
+            )
+            arWayag.add(data)
+        }
+    }
+    fun TampilkanData(){
+        _rvWayang.layoutManager = LinearLayoutManager(this)
+        _rvWayang.adapter = adapterRecView(arWayag)
+    }
+}
---
+++ app/src/main/java/c14220131/paba/recyclerview/wayang.kt
@@ -0,0 +1,8 @@
+package c14220131.paba.recyclerview
+
+data class wayang(
+    var foto:String,
+    var nama:String,
+    var karakter : String,
+    var deskripsi : String
+)
